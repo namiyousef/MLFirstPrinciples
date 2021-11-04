ed7a23264d1f568d0f12b03a05541d3076e85dbb
Yousef <namiyousef@hotmail.com>
2021-11-04 22:35:40 +0100

    updated plotting functions with new contour and plot_classes methods

--- PythonFiles/plotting.py
+++ PythonFiles/plotting.py
@@ -38,7 +38,7 @@ def gridspacer(*gridparams, mu = [0,0]):
     return lines, X_grid, axes
 
 
-def plot_classes(X,y, vars = None, **kwargs):
+def plot_classes_old(X,y, vars = None, **kwargs):
 
     """
     Given X and y, where X represents the X values and y the classes, plots outcomes
@@ -85,3 +85,79 @@ def reshape_by_component(f, *x):
     reshapes a function f by the dimensions of it's consituent components *x
     """
     return f.reshape(*[len(i) for i in x])
+
+
+def plot_classes(X, y, plot = None):
+    """
+    Plot 2 dimensional array X with corresponding y (classes) in different colours
+
+    X : np.array
+        X data, must be m rows by 2 dims
+
+    y : np.array
+        y data, must be discrete classes
+
+    plot : tuple
+        contains fig, ax from matplotlib. (fig, ax)
+    """
+    if not plot:
+        fig, ax = plt.subplots()
+    else:
+        fig, ax = plot
+    colors = ['C0', 'C1'] # TODO generalise this for many classes
+    for c, color in zip(np.unique(y), colors):
+        # loop through all possible classes, in this case 0 and 1
+        ax.plot(
+            X[y == c, 0],
+            X[y == c, 1],
+            '.',
+            c=color
+        )
+    return fig, ax
+
+
+
+
+def plot_contour(model, n = 100, xlim = (0,1), ylim = None, plot = None):
+    """
+    Plots contour plot
+
+    model :
+        fitted model that uses the sklearn API style
+
+    n : int
+        defines granularity of the contour
+        Can be a tuple, to determine x1 and x2 granuality separately
+    xlim : tuple
+        defines range of the plot
+
+    ylim : tuple
+        defines separate range for the plot if needed
+
+    plot : tuple
+        Defaults to None. Add fig, ax from matplotlib if you wish to overlay on another plot
+    """
+    lims = (xlim, ylim if ylim else xlim)
+    n = (n, n) if isinstance(n, int) else n
+
+
+    x = [np.linspace(*lim, n) for lim, n in zip(lims, n)]
+
+    X_grid = np.meshgrid(*x)
+
+    X_grid = np.asarray([x_grid.reshape(-1) for x_grid in X_grid]).T
+
+
+    if not plot:
+        fig, ax = plt.subplots()
+    else:
+        fig, ax = plot
+
+    y_hat = model.predict(np.ascontiguousarray(X_grid))
+
+    CS = ax.contourf(
+    *x,
+    np.asarray(y_hat).reshape(*n), # reshape to assign each y_hat to correct pair of points (x,x)
+           )
+
+    return fig, ax
